f73fabec04a76cda48be68187e9080dfd2462238
FelixFanPenn <fankejin@seas.upenn.edu>
2018-01-20 15:12:08 -0500

    added LastFM API calls

---
+++ app/src/main/java/pennapps/com/kukuoke/ArtistSearchJob.java
@@ -0,0 +1,82 @@
+package com.example.felix.lastfm;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Felix on 1/20/18.
+ */
+
+public class ArtistSearchJob extends AsyncTask<String, String, JSONArray> {
+    @Override
+    protected JSONArray doInBackground(String ... strings) {
+        Log.d("ARTIST1", strings[0]);
+        String root = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + strings[0] + "&autocorrect=1&api_key=f77bf3071f17ab92415497d1cb231590&format=json";
+        Log.d("ARTIST2", root);
+
+        try {
+            URL url = new URL(root);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            StringBuilder sb = new StringBuilder();
+
+            InputStreamReader in;
+            try {
+                in = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader br = new BufferedReader(in);
+
+                String line = "";
+
+                while (true){						// second while loop reads in all the robots txt content
+                    try {
+                        if (line != null){// && line.length() != 0){
+                            line = br.readLine();
+                            sb.append(line + "\n");
+                        } else {
+                            break;
+                        }
+                    } catch (IOException e) {
+                        break;
+                    }
+                }
+
+
+                br.close();
+                Log.d("/////////////", sb.toString());
+
+                try {
+                    JSONObject jo = new JSONObject(sb.toString());
+                    JSONObject artistmatches = (JSONObject) ((JSONObject) jo.get("results")).get("artistmatches");
+                    JSONArray ja = (JSONArray) artistmatches.get("artist");
+
+                    return ja;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (FileNotFoundException e) {
+                Log.d("File Not Found", e.getMessage());
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.d("Malformed", e.getMessage());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            Log.d("IOException", e1.getMessage());
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/pennapps/com/kukuoke/MainActivityAPI.java
@@ -0,0 +1,200 @@
+package com.example.felix.lastfm;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //TextView tv;
+    //ImageView iv;
+    public String picPath;
+    EditText trackSearch;
+    Button trackSearchBtn;
+    EditText artistSearch;
+    Button artistSearchBtn;
+    EditText albumSearch;
+    Button albumSearchBtn;
+    Button getSimilarTrackBtn;
+    Button getSimilarArtistBtn;
+
+    Button getArtistInfoBtn;
+    Button getTrackInfoBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trackSearch = (EditText) findViewById(R.id.trackSearch);
+        trackSearchBtn = (Button) findViewById(R.id.trackSearchBtn);
+        trackSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String trackName = trackSearch.getText().toString();
+                try {
+                    JSONArray ja = new TrackSearchJob().execute(trackName).get();
+                    Log.d("track json length", "" + ja.length());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        artistSearch = (EditText) findViewById(R.id.artistSearch);
+        artistSearchBtn = (Button) findViewById(R.id.artistSearchBtn);
+        artistSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String artistName = artistSearch.getText().toString();
+                try {
+                    JSONArray ja = new ArtistSearchJob().execute(artistName).get();
+                    Log.d("artist json length", "" + ja.length());
+                    Log.d("first artist name", ((JSONObject)ja.get(0)).get("name").toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        albumSearch = (EditText) findViewById(R.id.albumSearch);
+        albumSearchBtn = (Button) findViewById(R.id.albumSearchBtn);
+        albumSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String albumName = albumSearch.getText().toString();
+                try {
+                    JSONArray ja = new AlbumSearchJob().execute(albumName).get();
+                    Log.d("album json length", "" + ja.length());
+                    Log.d("first album name", ((JSONObject)ja.get(0)).get("name").toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        getSimilarTrackBtn = (Button) findViewById(R.id.getSimilarTrack);
+        getSimilarTrackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String artistName = artistSearch.getText().toString();
+                String trackName = trackSearch.getText().toString();
+                try {
+                    JSONArray ja = new SimilarTrackJob().execute(artistName, trackName).get();
+                    Log.d("similar tracks json len", "" + ja.length());
+                    Log.d("first track name", ((JSONObject)ja.get(0)).get("name").toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+        getSimilarArtistBtn = (Button) findViewById(R.id.getSimilarArtist);
+        getSimilarArtistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String artistName = artistSearch.getText().toString();
+                try {
+                    JSONArray ja = new SimilarArtistJob().execute(artistName).get();
+                    Log.d("sim artists json len", "" + ja.length());
+                    Log.d("first artist name", ((JSONObject)ja.get(0)).get("name").toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+        getArtistInfoBtn = (Button) findViewById(R.id.getArtistInfo);
+        getArtistInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String artistName = artistSearch.getText().toString();
+                try {
+                    JSONObject jo = new ArtistInfoJob().execute(artistName).get();
+                    Log.d("artist bio", ((JSONObject)jo.get("bio")).get("summary").toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        getTrackInfoBtn = (Button) findViewById(R.id.getTrackInfo);
+        getTrackInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String trackName = trackSearch.getText().toString();
+                String artistName = artistSearch.getText().toString();
+                try {
+                    JSONObject jo = new TrackInfoJob().execute(trackName, artistName).get();
+                    Log.d("duration is ", jo.get("duration").toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        /*
+        tv = (TextView) findViewById(R.id.textView);
+        tv.setText("hahahahah");
+        try {
+            picPath = new SendRequest().execute().get();
+            Log.d("~~~~~~~~~~~~~", picPath);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        iv = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this).load(picPath).into(iv);
+        */
+    }
+}

095cb90fa5d94d218c97a06bdd67ac8fe56c713c
oliviahw <oliviahw@seas.upenn.edu>
2018-01-21 05:17:14 -0500

    added friends activity

--- app/src/main/java/pennapps/com/kukuoke/FriendsActivity.java
+++ app/src/main/java/pennapps/com/kukuoke/FriendsActivity.java
@@ -1,17 +1,113 @@
 package pennapps.com.kukuoke;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class FriendsActivity extends AppCompatActivity {
+
+    ScrollView sv;
+    Button compareBtn;
+    ArrayList<CheckBox> arrCB;
+    EditText friendUID;
+    Button addFriendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+        arrCB = new ArrayList<>();
+
+        sv = (ScrollView) findViewById(R.id.scrollFriends);
+
+        FirebaseDatabase.getInstance().getReference().child("users").child(MainActivity.FBU.getUid()).child("friends").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                sv.removeAllViews();
+                LinearLayout linLayout = new LinearLayout(FriendsActivity.this);
+                linLayout.setOrientation((LinearLayout.VERTICAL));
+
+                Iterator<DataSnapshot> itr = dataSnapshot.getChildren().iterator();
+
+                while (itr.hasNext()) {
+                    final String s = itr.next().getValue().toString();
+                    final CheckBox cb = new CheckBox(FriendsActivity.this);
+
+                    FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String name = dataSnapshot.child(s).child("name").getValue().toString();
+                            String email = dataSnapshot.child(s).child("email").getValue().toString();
+                            Log.d("NAME", name);
+                            Log.d("EMAIL", email);
+                            cb.setText(email + " " + name);
+                            cb.setTextColor(Color.GREEN);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    arrCB.add(cb);
+                    linLayout.addView(cb);
+                }
+
+                sv.addView(linLayout);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        compareBtn = (Button) findViewById(R.id.compareBtn);
+        compareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (CheckBox cb : arrCB) {
+                    if (cb.isChecked()) {
+                        Log.d("CCCCCHECKBOX", cb.getText().toString());
+                    }
+                }
+            }
+        });
+
+        friendUID = (EditText) findViewById(R.id.frienduid);
+        addFriendBtn = (Button) findViewById(R.id.addFriendsBtn);
+        addFriendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String frienduid = friendUID.getText().toString();
+                FirebaseDatabase.getInstance().getReference().child("users").child(MainActivity.FBU.getUid()).child("friends").push().setValue(frienduid);
+            }
+        });
+
     }
 
     @Override
